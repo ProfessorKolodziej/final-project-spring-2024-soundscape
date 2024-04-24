76e67a574e2f69ac2406a421e0e1b8c763ad9c7c
simonYu_506 <149754712+simonyu97015@users.noreply.github.com>
2024-04-23 23:36:54 -0400

    current progress

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,27 @@
-// This is where you should write all JavaScript
-// for your project. Remember a few things as you start!
-// - Use let or const for all variables
-// - Do not use jQuery - use JavaScript instead
-// - Do not use onclick - use addEventListener instead
-// - Run npm run test regularly to check autograding
-// - You'll need to link this file to your HTML :)
+function toggleButton(element) {
+    element.classList.toggle('active');
+  }
+
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const openButton = document.getElementById('openModal');
+    const closeButton = document.getElementById('closeModal');
+    const modal = document.getElementById('searchModal');
+    const overlay = document.getElementById('modalOverlay');
+  
+    openButton.addEventListener('click', () => {
+      modal.classList.add('active');
+      overlay.classList.add('active');
+    });
+  
+    closeButton.addEventListener('click', () => {
+      modal.classList.remove('active');
+      overlay.classList.remove('active');
+    });
+  
+    overlay.addEventListener('click', () => {
+      modal.classList.remove('active');
+      overlay.classList.remove('active');
+    });
+  });
+  
