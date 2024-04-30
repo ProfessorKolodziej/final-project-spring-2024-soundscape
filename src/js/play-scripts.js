e800417c916f142bd6c4c1e4efcf9560ffba47f7
simonYu_506 <149754712+simonyu97015@users.noreply.github.com>
2024-04-30 17:53:34 -0400

    new

---
+++ src/js/play-scripts.js
@@ -0,0 +1,4 @@
+function playSound() {
+    var audio = new Audio('https://www.example.com/path-to-your-audio-file.mp3');
+    audio.play();
+  }
--- src/js/search-scripts.js
+++ src/js/search-scripts.js
@@ -1,15 +1,21 @@
 function toggleActive(element) {
-    // Remove 'active' class from all buttons
+  // If the element is already active, remove the active class
+  if (element.classList.contains('active')) {
+    element.classList.remove('active');
+  } else {
+    // Remove active class from all cards
     document.querySelectorAll('.category-card').forEach(function(card) {
       card.classList.remove('active');
     });
     // Add 'active' class to the clicked button
     element.classList.add('active');
   }
+}
 
-  function clearSelections() {
-    document.querySelectorAll('.category-card.active').forEach(function(card) {
-      card.classList.remove('active');
-    });
-  }
-  
+function clearSelections() {
+  // This will select all elements with the 'category-card' class that also have the 'active' class
+  document.querySelectorAll('.category-card.active').forEach(function(card) {
+    // Remove the 'active' class from each
+    card.classList.remove('active');
+  });
+}
